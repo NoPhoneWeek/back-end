15b6b20ada86a01edae0faed28ae1495e304f8ba
Dragomir Ioan <123ioandragomir123@gmail.com>
2016-06-04 22:00:50 +0000

    Oh boy more things that are coing to conflict >.<

--- lib/auth.rb
+++ lib/auth.rb
@@ -2,15 +2,14 @@ require_relative 'token_provider'
 require_relative 'user_login'
 
 class Authorization
-  @@login = Login.new()
-
+  @@login = Login.new() # Don't create a new Login instance everytime you auth somebody; have a static one
+    
   def initialize(auth_db)
     @auth_db = auth_db
   end
-    
+
   def authenticate(username, password)
-    id = @@login.check(username, password)
+    id = @@login.check(username, password) # I do this to cut the number of function calls in half
     return TokenProvider.issue_token({:id => id})  unless id.nil? 
   end
 end
-
